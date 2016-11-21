fb118254881a6dfd60559f2339ea7be407ada87e
Gabegit config --global user.email escobedo.gabe@my.codergv.org <gabriel.escobedo01@utrgv.edu>
2016-11-21 10:21:12 -0600

    Connected server to an HTML template file

--- mysalaryapp/server.js
+++ mysalaryapp/server.js
@@ -3,9 +3,9 @@
 var express = require('express');
 var app = express();
 
-app.get('/', function(req, res){
-  res.send("Hello world from server.js");
-});
+// Express command to tell the server where to look for (static files) the
+// index.html file (view/template for app)
+app.use(express.static(__dirname + "/public"));
 
 app.listen(3000);
 console.log("Server running on port 3000");
