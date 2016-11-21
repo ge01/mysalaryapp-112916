f08e25920d9c7000a4bb812ae4ad5ba4777927ac
Gabegit config --global user.email escobedo.gabe@my.codergv.org <gabriel.escobedo01@utrgv.edu>
2016-11-21 13:48:12 -0600

    Transferred dummy data from controller.js to the view

--- mysalaryapp/public/controllers/controller.js
+++ mysalaryapp/public/controllers/controller.js
@@ -1,4 +1,24 @@
 var myApp = angular.module('myApp', []);
 myApp.controller('AppCtrl', ['$scope', '$http', function($scope, $http) {
-    console.log("Hello World from controller");
+  console.log("Hello World from controller");
+
+  // Dummy Data
+  salary1 = {
+    annually: "24000",
+    monthly: "2000",
+    weekly: "462",
+    hourly: "11.54"
+  };
+  salary2 = {
+    annually: "34000",
+    monthly: "2833",
+    weekly: "654",
+    hourly: "16.35"
+  };
+
+  // Array with the dummy data objects transfered to the mysalry variable
+  var mysalary = [salary1, salary2];
+  // Put dummy data into index.html <table> using $scope
+  // Glue between between application controller and view/index.html
+  $scope.mysalary = mysalary;
 }]);
