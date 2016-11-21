1a564501c04808113ff67f73ddc50684bb59f776
Gabegit config --global user.email escobedo.gabe@my.codergv.org <gabriel.escobedo01@utrgv.edu>
2016-11-21 14:57:07 -0600

    Transferred dummy data from server.js to the view

--- mysalaryapp/public/controllers/controller.js
+++ mysalaryapp/public/controllers/controller.js
@@ -2,23 +2,13 @@ var myApp = angular.module('myApp', []);
 myApp.controller('AppCtrl', ['$scope', '$http', function($scope, $http) {
   console.log("Hello World from controller");
 
-  // Dummy Data
-  salary1 = {
-    annually: "24000",
-    monthly: "2000",
-    weekly: "462",
-    hourly: "11.54"
-  };
-  salary2 = {
-    annually: "34000",
-    monthly: "2833",
-    weekly: "654",
-    hourly: "16.35"
-  };
+  // Route to get dummy data from server.js
+  // Sends a request to the server
+  $http.get('/mysalary').success(function(response){
+    console.log("I got the data I requested");
 
-  // Array with the dummy data objects transfered to the mysalry variable
-  var mysalary = [salary1, salary2];
-  // Put dummy data into index.html <table> using $scope
-  // Glue between between application controller and view/index.html
-  $scope.mysalary = mysalary;
+    // Put the data into index.html
+    $scope.mysalary = response;
+  });
+
 }]);
--- mysalaryapp/server.js
+++ mysalaryapp/server.js
@@ -7,5 +7,31 @@ var app = express();
 // index.html file (view/template for app)
 app.use(express.static(__dirname + "/public"));
 
+// Respond to the controllers request by sending dummy data
+app.get('/mysalary', function(req, res){
+  console.log("I received a GET request");
+
+  // Dummy Data
+  salary1 = {
+    annually: "24000",
+    monthly: "2000",
+    weekly: "462",
+    hourly: "11.54"
+  };
+  salary2 = {
+    annually: "34000",
+    monthly: "2833",
+    weekly: "654",
+    hourly: "16.35"
+  };
+
+  // Array with the dummy data objects transfered to the mysalry variable
+  var mysalary = [salary1, salary2];
+
+  // Respond to the Get request by sending back the mysalary data in JSON format
+  // which the controller can use
+  res.json(mysalary);
+});
+
 app.listen(3000);
 console.log("Server running on port 3000");
